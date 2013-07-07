10b195833af54c7e917b98ff5bc6f504c9c3146e
Ubuntu <ubuntu@ip-172-31-42-115.us-west-2.compute.internal>
2013-07-07 02:40:41 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs=require('fs');
+
 app.get('/', function(request, response) {
 	var archivo = fs.readFile('index.html', 'utf-8');
 	buf = new Buffer(archivo, 'utf-8');
